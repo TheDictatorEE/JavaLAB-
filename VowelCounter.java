2f843933a5c761ebe82cf0508c1c106ac6011bae
TheDictatorEE <gauravchoudhary1910@gmail.com>
2025-05-23 12:42:03 +0530

    Add files via upload

---
+++ VowelCounter.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class VowelCounter {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine().toLowerCase();
+        int count = 0;
+
+        for (char ch : input.toCharArray()) {
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                count++;
+            }
+        }
+
+        System.out.println("Number of vowels: " + count);
+    }
+}
